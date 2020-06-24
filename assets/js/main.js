a08fa5c63b9e4e1a4450a8e2b3033ae6f40f43a6
abisheksrm <abisheksrm@gmail.com>
2020-06-24 18:18:42 +0530

    new add

---
+++ assets/js/main.js
@@ -0,0 +1,287 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("container-block1");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+}
+
+$("#draggable2").draggable({
+  stop: function (event, ui) {
+    var target = document.querySelector("#draggable" + slideIndex);
+    target.style.top = 0;
+    target.style.left = 0;
+    target.style.right = 0;
+    target.style.bottom = 0;
+    target.style.opacity = 1;
+  },
+  drag: function (event, ui) {
+    var target = document.querySelector("#draggable" + slideIndex);
+    target.style.opacity = 0.5;
+  },
+});
+$("#draggable3").draggable({
+  stop: function (event, ui) {
+    var target = document.querySelector("#draggable" + slideIndex);
+    target.style.top = 0;
+    target.style.left = 0;
+    target.style.right = 0;
+    target.style.bottom = 0;
+    target.style.opacity = 1;
+  },
+  drag: function (event, ui) {
+    var target = document.querySelector("#draggable" + slideIndex);
+    target.style.opacity = 0.5;
+  },
+});
+
+$("#droppable2").droppable({
+  drop: function (ui, event) {
+    $(".block1b-image").attr(
+      "src",
+      "assets/img/ContactPlus/Balaji%20Precautions-13.svg"
+    );
+    $("#active_d_2").removeClass("active_2").addClass("active_1");
+    $("#active_d_2_m").removeClass("active_2").addClass("active_1");
+    $(".box2").css("height", "210px");
+    $("#draggable" + slideIndex).hide();
+    $(".block1b-text-container" + slideIndex).css("display", "block");
+  },
+});
+$("#droppable3").droppable({
+  drop: function (ui, event) {
+    $(".block1b-image").attr(
+      "src",
+      "assets/img/ContactPlus/Balaji%20Precautions-11.svg"
+    );
+    $("#active_d_3").removeClass("active_2").addClass("active_1");
+    $("#active_d_3_m").removeClass("active_2").addClass("active_1");
+    $(".box3").css("height", "210px");
+    $("#draggable" + slideIndex).hide();
+    $(".block1b-text-container" + slideIndex).css("display", "block");
+  },
+});
+
+$(document).ready(function () {
+  var divs = $(".mydivs>.d_d_div");
+  var now = 0; // currently shown div
+  divs.hide().first().show(); // hide all divs except first
+  $("button[name=next]").click(function () {
+    divs.eq(now).hide();
+    now = now + 1 < divs.length ? now + 1 : 0;
+    divs.eq(now).show(); // show next
+  });
+  $("button[name=prev]").click(function () {
+    divs.eq(now).hide();
+    now = now > 0 ? now - 1 : divs.length - 1;
+    divs.eq(now).show(); // show previous
+  });
+});
+var dropped = false;
+$("#drag").draggable({
+  helper: "original",
+  revert: "invalid",
+  drag: function (event, ui) {
+    var target = document.getElementById("drag");
+    target.style.width = "35%";
+    target.style.top = 0;
+    target.style.left = 0;
+    target.style.right = 0;
+    target.style.bottom = 0;
+    target.style.opacity = 1;
+    $("#drag_img").attr(
+      "src",
+      "assets/img/ContactPlus/Balaji%20Precautions-13.svg"
+    );
+    document.getElementById("drag_img").style.display = "block";
+  },
+  stop: function (event) {
+    if (!dropped) {
+      var target = document.getElementById("drag");
+      target.style.opacity = 0;
+      $("#drag_img").attr(
+        "src",
+        "assets/img/ContactPlus/Balaji%20Precautions-11.svg"
+      );
+    }
+  },
+});
+$("#drag").draggable({
+  cursor: "move",
+  cursorAt: { width: "50%", height: "50%" },
+  helper: function (event) {
+    return $("#drag").attr("src", "assets/img/Assets/Gloves.svg");
+  },
+});
+$("#drop").droppable({
+  drop: function (event, ui) {
+    $("#drop").html('<img src="assets/img/Assets/Gloves.svg">');
+    $("#drag").hide();
+    $("#drag_img").attr(
+      "src",
+      "assets/img/ContactPlus/Balaji%20Precautions-13.svg"
+    );
+    $("#active_r_1").removeClass("active_2").addClass("active_1");
+    $("#active_r_1_m").removeClass("active_2").addClass("active_1");
+    dropped = true;
+    var target = document.getElementById("drop");
+    target.style.opacity = 0.5;
+    document.getElementById("drop_drag_next_nex").style.display = "block";
+    document.getElementById("drop_drag_next_con").style.display = "block";
+  },
+});
+
+var dropped1 = false;
+$("#drag_1").draggable({
+  helper: "original",
+  revert: "invalid",
+  drag: function (event, ui) {
+    var target = document.getElementById("drag_1");
+    target.style.width = "25%";
+    target.style.top = 0;
+    target.style.left = 0;
+    target.style.right = 0;
+    target.style.bottom = 0;
+    target.style.opacity = 1;
+    $("#drag_img_1").attr(
+      "src",
+      "assets/img/ContactPlus/Balaji%20Precautions-14.svg"
+    );
+    document.getElementById("drag_img_1").style.display = "block";
+  },
+  stop: function (event) {
+    if (!dropped1) {
+      var target = document.getElementById("drag_1");
+      target.style.opacity = 0;
+      $("#drag_img_1").attr(
+        "src",
+        "assets/img/ContactPlus/Balaji%20Precautions-13.svg"
+      );
+    }
+  },
+});
+$("#drag_1").draggable({
+  cursor: "move",
+  cursorAt: { width: "50%", height: "50%" },
+  helper: function (event) {
+    return $("#drag_1").attr("src", "assets/img/Assets/Gown.svg");
+  },
+});
+$("#drop_1").droppable({
+  drop: function (event, ui) {
+    $("#drop_1").html('<img src="assets/img/Assets/Gown.svg">');
+    $("#drag_1").hide();
+    $("#active_r_2").removeClass("active_2").addClass("active_1");
+    $("#active_r_2_m").removeClass("active_2").addClass("active_1");
+    dropped1 = true;
+    var target = document.getElementById("drop_1");
+    target.style.opacity = 0.5;
+    document.getElementById("drop_drag_next_nex_1").style.display = "block";
+    document.getElementById("drop_drag_next_con_1").style.display = "block";
+  },
+});
+
+var dropped2 = false;
+$("#drag_2").draggable({
+  helper: "original",
+  revert: "invalid",
+  drag: function (event, ui) {
+    var target = document.getElementById("drag_2");
+    target.style.width = "30%";
+    target.style.top = 0;
+    target.style.left = 0;
+    target.style.right = 0;
+    target.style.bottom = 0;
+    target.style.opacity = 1;
+    $("#drag_img_2").attr(
+      "src",
+      "assets/img/ContactPlus/Balaji%20Precautions-14.svg"
+    );
+    document.getElementById("drag_img_2").style.display = "block";
+  },
+  stop: function (event) {
+    if (!dropped2) {
+      var target = document.getElementById("drag_2");
+      target.style.opacity = 0;
+      $("#drag_img_2").attr(
+        "src",
+        "assets/img/Droplet_Character/After/Character_Apron%20Dispose-03.svg"
+      );
+    }
+  },
+});
+
+$("#drag_d_1").draggable({
+  helper: "original",
+  revert: "invalid",
+  drag: function (event, ui) {
+    var target = document.getElementById("drag_d_1");
+
+    target.style.top = 0;
+    target.style.left = 0;
+    target.style.right = 0;
+    target.style.bottom = 0;
+    target.style.opacity = 1;
+  },
+});
+$("#drop_d_1").droppable({
+  drop: function (event, ui) {
+    $("#drag_img_d_1").css("display", "block");
+    $("#drop_d_1").hide();
+    $("#drag_d_1").hide();
+    $(".block1b-text-container").css("display", "block");
+    $("#active_d_1").removeClass("active_2").addClass("active_1");
+    $("#active_d_1_m").removeClass("active_2").addClass("active_1");
+  },
+});
+$("#drag_d_2").draggable({
+  helper: "original",
+  revert: "invalid",
+  drag: function (event, ui) {
+    var target = document.getElementById("drag_d_2");
+
+    target.style.top = 0;
+    target.style.left = 0;
+    target.style.right = 0;
+    target.style.bottom = 0;
+    target.style.opacity = 1;
+  },
+});
+$("#drop_d_2").droppable({
+  drop: function (event, ui) {
+    $("#drag_img_d_2").css("display", "block");
+    $("#drop_d_2").hide();
+    $("#drag_d_2").hide();
+    $("#active_r_4").removeClass("active_2").addClass("active_1");
+    $("#active_r_4_m").removeClass("active_2").addClass("active_1");
+    var target = document.getElementById("drop_d_2");
+    document.getElementById("drop_drag_next_con_d_2").style.display = "block";
+    document.getElementById("drop_drag_next_nex_d_2").style.display = "block";
+  },
+});
+
+if ($(window).width() > 600) {
+  $(".rwl_1").addClass("rwl");
+} else {
+  $(".rwl_1").removeClass("rwl");
+}
